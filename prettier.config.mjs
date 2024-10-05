c79ff4ce4aa6d3e181d673de10be4284669520ca
iamryanmacdonald <ram_idts@me.com>
2024-10-05 14:43:27 +1000

    added authentication

---
+++ prettier.config.mjs
@@ -0,0 +1,30 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+  arrowParens: "always",
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^[.|..|@]",
+    "^@/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  printWidth: 80,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+};
+
+export default config;
